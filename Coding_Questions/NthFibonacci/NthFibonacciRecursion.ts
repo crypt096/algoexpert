79bb0ecfc401fe3953adcfbf50ddc88ce4faf23e
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-01-07 21:59:55 +0100

    Add solutions

---
+++ Coding_Questions/NthFibonacci/NthFibonacciRecursion.ts
@@ -0,0 +1,9 @@
+export function getNthFib(n: number):number {
+  if(n < 1) {
+		return 0;
+	} else if ( n === 2) {
+		return 1;
+	} else {
+		return getNthFib(n-1) + getNthFib(n-2);
+	}
+}
---
+++ Coding_Questions/NthFibonacci/NthFibonacciRecursionWithMemoization.ts
@@ -0,0 +1,12 @@
+interface Cache {
+	[key: number]: number;
+}
+
+export function getNthFib(n: number, memoize: Cache = {1: 0, 2: 1}){
+	if(n in memoize) {
+		return memoize[n]
+	} else {
+		memoize[n] = getNthFib(n-1, memoize) + getNthFib(n-2, memoize);
+		return memoize[n];
+	}
+}
