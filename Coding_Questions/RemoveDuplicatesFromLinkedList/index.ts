93f7a69079c70b58221c984853dd8ecb850aad4d
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-09-28 18:19:43 +0200

    Create index.ts

---
+++ Coding_Questions/RemoveDuplicatesFromLinkedList/index.ts
@@ -0,0 +1,26 @@
+// This is an input class. Do not edit.
+export class LinkedList {
+  value: number;
+  next: LinkedList | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export function removeDuplicatesFromLinkedList(linkedList: LinkedList) {
+  let currentNode: LinkedList | null = linkedList;
+  let nextNode: LinkedList | null = linkedList.next;
+
+  while(!!currentNode) {
+    if(currentNode.value !== nextNode?.value) {
+      currentNode.next = nextNode;
+      currentNode = nextNode;
+    }
+
+    nextNode = nextNode?.next || null;
+  }
+
+  return linkedList;
+}
