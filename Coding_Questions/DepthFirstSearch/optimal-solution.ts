0231395b88600bb621e1f1d1329e00846e1a5545
Alex <31933549+crypt096@users.noreply.github.com>
2023-12-19 22:33:50 +0100

    Add DFS

---
+++ Coding_Questions/DepthFirstSearch/optimal-solution.ts
@@ -0,0 +1,24 @@
+export class Node {
+  name: string;
+  children: Node[];
+
+  constructor(name: string) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name: string) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array: string[]) {
+    array.push(this.name);
+
+    this.children.forEach((childNode: Node) => {
+      childNode.depthFirstSearch(array)
+    });
+
+    return array;
+  }
+}
