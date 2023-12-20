0db86ad936971134efd0fa336c92f608d7c548ea
Alex <31933549+crypt096@users.noreply.github.com>
2023-12-20 18:49:59 +0100

    Create optimal-solution.ts

---
+++ Coding_Questions/BreadthFirstSearch/optimal-solution.ts
@@ -0,0 +1,30 @@
+export class Node {
+  name: string;
+  children: Node[];
+
+  constructor(name: string) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name: string): Node {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  breadthFirstSearch(array: string[]) {
+    const queue: Node[] = [this];
+
+    while (queue.length) {
+      let current: Node | undefined = queue.shift();
+      if (current) {
+        array.push(current.name);
+        current.children.forEach((child: Node) => {
+          queue.push(child);
+        })
+      }
+    }
+
+    return array;
+  }
+}
