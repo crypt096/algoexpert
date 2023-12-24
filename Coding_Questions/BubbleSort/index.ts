aff57c0be772c25f418bfcb8c0d8f052d0c98b39
Alex <31933549+crypt096@users.noreply.github.com>
2023-12-24 21:49:03 +0100

    Update index.ts

--- Coding_Questions/BubbleSort/index.ts
+++ Coding_Questions/BubbleSort/index.ts
@@ -1,17 +1,24 @@
 export function bubbleSort(array: number[]) {
-  for(let i = 0; i < array.length; i++) {
-    for(let j = 0; j < array.length - 1; ++j) {
-      if(array[j] > array[j+1]) {
-        swap(array, j, j+1);
+  let isSorted = false;
+  let counter = 0;
+
+  while(!isSorted) {
+    isSorted = true;
+
+    for(let i = 0; i < array.length - 1 - counter; i++) {
+      if (array[i] > array[i+1]) {
+        swap(i, i+1, array);
+        isSorted = false;
       }
     }
+    counter++;
   }
 
   return array;
 }
 
-function swap(arr: number[], currentIndex: number, nextIndex: number) {
-  const temp = arr[currentIndex];
-  arr[currentIndex] = arr[nextIndex];
-  arr[nextIndex] = temp;
+function swap(i: number, j: number, array: number[]) {
+  const temp = array[j];
+  array[j] = array[i];
+  array[i] = temp;
 }
