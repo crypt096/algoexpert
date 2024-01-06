4cab5cc2f5aa22e9ecde911b381393772bd6ee8e
Alex <31933549+crypt096@users.noreply.github.com>
2024-01-06 18:10:59 +0100

    Create recursive-solution.ts

---
+++ Coding_Questions/BinaryTrees/BranchSums/recursive-solution.ts
@@ -0,0 +1,33 @@
+// This is the class of the input root.
+// Do not edit it.
+class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export function branchSums(root: BinaryTree): number[] {
+  const sums: number[] = [];
+  calculateBranchSums(root, 0, sums);  
+  return sums;
+}
+
+function calculateBranchSums(node: BinaryTree | null, runningSum: number, sums: number[]) {
+  if(!node) return;
+
+  const newRunningSum = runningSum + node.value;
+
+  if(!node.left && !node.right) {
+    sums.push(newRunningSum);
+    return;
+  }
+
+  calculateBranchSums(node.left, newRunningSum, sums);
+  calculateBranchSums(node.right, newRunningSum, sums);
+}
