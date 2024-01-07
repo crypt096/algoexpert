0b491835e92579e8004eed17d3bd28a1cc0da873
Alex <31933549+crypt096@users.noreply.github.com>
2024-01-07 19:47:03 +0100

    Update recursive-solution.ts

--- Coding_Questions/Recursion/NthFibonacci/recursive-solution.ts
+++ Coding_Questions/Recursion/NthFibonacci/recursive-solution.ts
@@ -1,9 +1,9 @@
 export function getNthFib(n: number):number {
-  if(n < 1) {
-		return 0;
-	} else if ( n === 2) {
-		return 1;
-	} else {
-		return getNthFib(n-1) + getNthFib(n-2);
-	}
+  if (n === 1) {
+    return 0;
+  } else if (n === 2) {
+    return 1;
+  } else {
+    return getNthFib(n-1) + getNthFib(n-2);
+  }
 }
