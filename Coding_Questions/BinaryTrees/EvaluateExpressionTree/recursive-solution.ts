122c984e6f11d145eda70e470c83d058191a2ef1
Alex <31933549+crypt096@users.noreply.github.com>
2024-01-10 20:22:16 +0100

    Create recursive-solution.ts

---
+++ Coding_Questions/BinaryTrees/EvaluateExpressionTree/recursive-solution.ts
@@ -0,0 +1,25 @@
+// This is an input class. Do not edit.
+export class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export function evaluateExpressionTree(tree: BinaryTree): number {
+  if (tree.value >= 0) return tree.value;
+
+  const leftValue = evaluateExpressionTree(tree.left!);
+  const rightValue = evaluateExpressionTree(tree.right!);
+
+  if (tree.value === -1) return leftValue + rightValue;
+  if (tree.value === -2) return leftValue - rightValue;
+  if (tree.value === -3) return Math.trunc(leftValue / rightValue);
+
+  return leftValue * rightValue;
+}
