f03cdd7a236e78972cd9b0495fd94e2821942baa
Alex <31933549+crypt096@users.noreply.github.com>
2024-01-25 19:35:43 +0100

    Create solution.ts

---
+++ Coding_Questions/LinkedLists/MergingLinkedLists/solution.ts
@@ -0,0 +1,32 @@
+// This is an input class. Do not edit.
+export class LinkedList {
+  value: number;
+  next: LinkedList | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// O(n + m) time | O(1) space - where n is the length of the first linked list and m is the length of the second linked list
+export function mergingLinkedLists(linkedListOne: LinkedList, linkedListTwo: LinkedList) {
+  let currentNodeOne: LinkedList | null = linkedListOne;
+  let currentNodeTwo: LinkedList | null = linkedListTwo;
+
+  while (currentNodeOne !== currentNodeTwo) {
+    if (currentNodeOne === null) {
+      currentNodeOne = linkedListTwo;
+    } else {
+      currentNodeOne = currentNodeOne.next;
+    }
+
+    if (currentNodeTwo === null) {
+      currentNodeTwo = linkedListOne;
+    } else {
+      currentNodeTwo = currentNodeTwo.next;
+    }
+  }
+
+  return currentNodeOne;
+}
