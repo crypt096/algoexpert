1637c42a1eb1f309f1c1c724065ac70ca865f37a
Alex <31933549+crypt096@users.noreply.github.com>
2024-01-25 18:21:18 +0100

    Create solution.ts

---
+++ Coding_Questions/LinkedLists/RemoveKthNodeFromEnd/solution.ts
@@ -0,0 +1,33 @@
+export class LinkedList {
+  value: number;
+  next: LinkedList | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export function removeKthNodeFromEnd(head: LinkedList, k: number) {
+  let counter = 1;
+  let firstNode: LinkedList = head;
+  let secondNode: LinkedList | null = head;
+
+  while (counter <= k) {
+    secondNode = secondNode!.next;
+    counter++;
+  }
+
+  if (secondNode === null) {
+    head.value = head.next!.value;
+    head.next = head.next!.next;
+    return;
+  }
+
+  while (secondNode.next !== null) {
+    secondNode = secondNode.next;
+    firstNode = firstNode.next!;
+  }
+
+  firstNode.next = firstNode.next!.next;
+}
