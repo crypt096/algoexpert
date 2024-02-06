199d0450113bae3ffb51e77a65a9caabe2e5e669
Alex <31933549+crypt096@users.noreply.github.com>
2024-02-06 19:55:25 +0100

    Create solution.ts

---
+++ Coding_Questions/Arrays/SmallestDifference/solution.ts
@@ -0,0 +1,32 @@
+export function smallestDifference(arrayOne: number[], arrayTwo: number[]) {
+  arrayOne.sort((a, b) => a - b);
+  arrayTwo.sort((a, b) => a - b);
+
+  let idxOne = 0;
+  let idxTwo = 0;
+  let smallest = Infinity;
+  let current = Infinity;
+  let smallestPair: number[] = [];
+
+  while (idxOne < arrayOne.length && idxTwo < arrayTwo.length) {
+    let firstNum = arrayOne[idxOne];
+    let secondNum = arrayTwo[idxTwo];
+
+    if (firstNum < secondNum) {
+      current = secondNum - firstNum;
+      idxOne++;
+    } else if (secondNum < firstNum) {
+      current = firstNum - secondNum;
+      idxTwo++;
+    } else {
+      return [firstNum, secondNum];
+    }
+
+    if (smallest > current) {
+      smallest = current;
+      smallestPair = [firstNum, secondNum];
+    }
+  }
+
+  return smallestPair;
+}
